6fa27f6daeb8bf333aa21504dc311e87fca8400a
zjlai <zj.lai21@gmail.com>
2018-04-11 13:48:01 +0800

    Added student list

--- src/pages/assignclass.vue
+++ src/pages/assignclass.vue
@@ -15,16 +15,18 @@
         </q-list>
       </div>
       <div class="col">
-        <q-list highlight sparse separator link>
-          <q-list-header>Students</q-list-header>
-          <q-item-separator />
-          <q-item v-for="student in students" :key="student.id">
-          <q-item-side>
-            <q-checkbox v-model="student.selected" toggle-indeterminate true-value="2" indeterminate-value="1" false-value="0" />
-          </q-item-side>
-            <q-item-main :label="student.name" />
-          </q-item>
-        </q-list>
+        <!--<q-scroll-area style="height:100%">-->
+          <q-list highlight sparse separator link>
+            <q-list-header>Students</q-list-header>
+            <q-item-separator />
+            <q-item v-for="student in students" :key="student.id" tag="label" @click.native="toggleStudent(group.id)">
+            <q-item-side>
+              <q-checkbox v-model="student.selected" true-value="1" false-value="0" />
+            </q-item-side>
+              <q-item-main :label="student.name" />
+            </q-item>
+          </q-list>
+        <!--</q-scroll-area>-->
       </div>
       <div class="col">
         <q-scroll-area>
@@ -50,32 +52,46 @@ export default {
       students: []
     }
   },
+  computed: {
+  },
   methods: {
     getStudents (groupid) {
+      var vm = this
+      var group = this.$_.find(this.groups, { 'id': groupid })
       this.students = []
-      console.log(groupid)
       setTimeout(function () {
-        this.returnStudents()
+        if (group.selected === '0') {
+          group.selected = '2'
+        }
+        vm.returnStudents()
       }, 1000)
     },
     returnStudents () {
-      console.log('returnStudents')
       this.students = [
-        { id: 0, name: 'Xiao Ming', selected: '2' },
-        { id: 1, name: 'Xiao Le', selected: '2' },
-        { id: 2, name: 'Ah Ren', selected: '2' },
-        { id: 3, name: 'Joan Lim', selected: '2' },
-        { id: 4, name: 'Christine Tan', selected: '2' },
-        { id: 5, name: 'Sape Ben', selected: '2' },
-        { id: 6, name: 'Gumon Kumon', selected: '2' },
-        { id: 7, name: 'John Lee', selected: '2' },
-        { id: 8, name: 'Noah Helm', selected: '2' },
-        { id: 9, name: 'Zachary Mess', selected: '2' },
-        { id: 10, name: 'Zhang Yi Yang', selected: '2' },
-        { id: 11, name: 'Chen Daxi', selected: '2' },
-        { id: 12, name: 'Julie Lie', selected: '2' },
-        { id: 12, name: 'Wah Tat On', selected: '2' }
+        { id: 0, name: 'Xiao Ming', selected: '1' },
+        { id: 1, name: 'Xiao Le', selected: '1' },
+        { id: 2, name: 'Ah Ren', selected: '1' },
+        { id: 3, name: 'Joan Lim', selected: '1' },
+        { id: 4, name: 'Christine Tan', selected: '1' },
+        { id: 5, name: 'Sape Ben', selected: '1' },
+        { id: 6, name: 'Gumon Kumon', selected: '1' },
+        { id: 7, name: 'John Lee', selected: '1' },
+        { id: 8, name: 'Noah Helm', selected: '1' },
+        { id: 9, name: 'Zachary Mess', selected: '1' },
+        { id: 10, name: 'Zhang Yi Yang', selected: '1' },
+        { id: 11, name: 'Chen Daxi', selected: '1' },
+        { id: 12, name: 'Julie Lie', selected: '1' },
+        { id: 13, name: 'Wah Tat On', selected: '1' }
       ]
+    },
+    toggleStudent (groupid) {
+      console.log('toggle')
+      var group = this.$_.find(this.groups, { 'id': groupid })
+      console.log(group)
+      var selectAll = this.$_.find(this.students, { 'selected': 0 })
+      if (selectAll.length !== this.students.length) {
+        console.log('not equal')
+      }
     }
   }
 }
